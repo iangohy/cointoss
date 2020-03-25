736e424cbee437b23ad38970a2916277bcd68d63
Ian <ian@iangohy.com>
2020-03-25 11:51:43 +0800

    Updates

---
+++ precache-manifest.63ae8b900ad8533df3166c6250c82f12.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e43ae5e1befafd32c0b940a7b50c347",
+    "url": "/cointoss/index.html"
+  },
+  {
+    "revision": "f1d91c1d2b11b909dcdd",
+    "url": "/cointoss/static/css/main.b675c773.chunk.css"
+  },
+  {
+    "revision": "80a672dac03be2ff6230",
+    "url": "/cointoss/static/js/2.5e5d1c5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cointoss/static/js/2.5e5d1c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d91c1d2b11b909dcdd",
+    "url": "/cointoss/static/js/main.b6e813ce.chunk.js"
+  },
+  {
+    "revision": "2bf8bfd94b71d4aacd21",
+    "url": "/cointoss/static/js/runtime-main.f62b64d2.js"
+  },
+  {
+    "revision": "f7128e0eb8fa4bca85146b2f32bea9b5",
+    "url": "/cointoss/static/media/TS10cb.f7128e0e.png"
+  },
+  {
+    "revision": "ed9f816b2a54e82daf017a461413ba73",
+    "url": "/cointoss/static/media/TS1d.ed9f816b.png"
+  },
+  {
+    "revision": "1769fff0f55958048c749c7dc0013493",
+    "url": "/cointoss/static/media/TS1db.1769fff0.png"
+  },
+  {
+    "revision": "086c271eb0db1debd22e18b5d11ad905",
+    "url": "/cointoss/static/media/TS20c.086c271e.png"
+  },
+  {
+    "revision": "f3c530f71649dd339be40eea9ce263cf",
+    "url": "/cointoss/static/media/TS20cb.f3c530f7.png"
+  },
+  {
+    "revision": "faa1fba1ac7b6157244ad1d61941ef80",
+    "url": "/cointoss/static/media/TS50c.faa1fba1.png"
+  },
+  {
+    "revision": "2c74274c1a824a7e8db850d4a76e8911",
+    "url": "/cointoss/static/media/TS50cb.2c74274c.png"
+  },
+  {
+    "revision": "664c2253621320e66104253d219e47f8",
+    "url": "/cointoss/static/media/TS5cb.664c2253.png"
+  }
+]);
